6cc0c5213ab924f6a7f31888ffdb8b9ede3d028f
Dominik Ucher <dominikucher@hotmail.com>
2024-01-11 11:44:22 +0100

    Some stuff added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
         <div className='text-center'>
           <FadeIn delay={300}>
           <h1 className='text-4xl md:text-8xl font-bold tracking-tighter text-white'>
-            Welcome to UcherTech
+            Welcome to RaindropCoding
           </h1>
           </FadeIn>
           <FadeIn delay={400}>
@@ -99,22 +99,15 @@ const Home = () => {
           <div className="flex flex-col lg:py-6 lg:pl-12 lg:text-left text-center">
             <div className="flex flex-col mb-10 lg:items-start items-center">
               <div className="flex-grow">
-                <h2 className="text-white text-2xl title-font font-medium mb-3">Innovative Designs</h2>
-                <p className="leading-relaxed text-lg">We specializes in creating cutting-edge designs that capture attention and leave a lasting impression.</p>
+                <h2 className="text-white text-2xl title-font font-medium mb-3">Digital Marketing</h2>
+                <p className="leading-relaxed text-lg">Our digital marketing services are designed to help you reach your target audience and grow your business. We offer a wide range of services, including search engine optimization (SEO), social media marketing, email marketing, and more. Our team of experts will work with you to create a customized marketing plan that meets your specific needs.</p>
               </div>
             </div>
 
             <div className="flex flex-col mb-10 lg:items-start items-center">
               <div className="flex-grow">
-                <h2 className="text-white text-2xl title-font font-medium mb-3">Affordable Solutions</h2>
-                <p className="leading-relaxed text-lg">We offer cost-effective solutions tailored to your budget, allowing businesses of all sizes to access top-quality web design and development services.</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col mb-10 lg:items-start items-center">
-              <div className="flex-grow">
-                <h2 className="text-white text-2xl title-font font-medium mb-3">User-Centric Approach</h2>
-                <p className="leading-relaxed text-lg">Our user-centric philosophy drives everything we do. We prioritize seamless user experiences, ensuring your website is intuitive, easy to navigate, and designed to convert visitors into customers.</p>
+                <h2 className="text-white text-2xl title-font font-medium mb-3">Website Designs</h2>
+                <p className="leading-relaxed text-lg">We specialize in creating custom website designs that are tailored to your business needs. Our team of designers will work with you to create a website that is both visually appealing and user-friendly. We use the latest design trends and technologies to ensure that your website is up-to-date and meets the needs of your customers.</p>
               </div>
             </div>
 
@@ -172,26 +165,26 @@ const Home = () => {
         <div className='grid grid-cols-3 gap-10 px-10 mt-10'>
           <div className='flex justify-center text-center md:col-span-1 col-span-3'>
             <div className="flex-grow">
-              <img src={Design} alt="" className='rounded w-60 mx-auto'/> {/* Added mx-auto */}
-              <h2 className="text-white text-2xl title-font font-medium mb-3 pt-10">Website Designs</h2>
+              <img src={Hosting} alt="" className='rounded w-60 mx-auto'/> {/* Added mx-auto */}
+              <h2 className="text-white text-2xl title-font font-medium mb-3 pt-10">Digital Marketing</h2>
               <div className='w-32 h-1 rounded-full bg-orange-400 mx-auto mt-5'></div>
-              <p className="leading-relaxed text-lg mt-5">We specialize in crafting budget-friendly websites that cater to your specific needs. Our skilled designers create visually appealing and user-friendly websites that effectively represent your brand and message.</p>
+              <p className="leading-relaxed text-lg mt-5">With our Digital Marketing service we can help increase your reach and popularity, aswell as grow your digital presence.</p>
             </div>
           </div>
           <div className='flex justify-center text-center md:col-span-1 col-span-3'>
             <div className="flex-grow">
-              <img src={Hosting} alt="" className='rounded w-60 mx-auto'/> {/* Added mx-auto */}
-              <h2 className="text-white text-2xl title-font font-medium mb-3 pt-10">Hosting Solutions</h2>
+              <img src={Design} alt="" className='rounded w-60 mx-auto'/> {/* Added mx-auto */}
+              <h2 className="text-white text-2xl title-font font-medium mb-3 pt-10">Website Designs</h2>
               <div className='w-32 h-1 rounded-full bg-orange-400 mx-auto mt-5'></div>
-              <p className="leading-relaxed text-lg mt-5">With our hosting services, you won't have to worry about the technical aspects of maintaining a website. We provide reliable hosting solutions that ensure your website is accessible 24/7, allowing your visitors to engage with your content seamlessly.</p>
+              <p className="leading-relaxed text-lg mt-5">We specialize in website designs custom to your needs. We make user friendly and websites that are both appealing and easy to use.</p>
             </div>
           </div>
           <div className='flex justify-center text-center md:col-span-1 col-span-3'>
             <div className="flex-grow">
               <img src={Domain} alt="" className='rounded w-60 mx-auto'/> {/* Added mx-auto */}
-              <h2 className="text-white text-2xl title-font font-medium mb-3 pt-10">Domain Management</h2>
+              <h2 className="text-white text-2xl title-font font-medium mb-3 pt-10">Digital Management</h2>
               <div className='w-32 h-1 rounded-full bg-orange-400 mx-auto mt-5'></div>
-              <p className="leading-relaxed text-lg mt-5"> We simplify the process of obtaining and managing domains. Whether you need a new domain or want to transfer an existing one, we handle the domain registration and management process, ensuring your website has a professional online presence.</p>
+              <p className="leading-relaxed text-lg mt-5"> We simplify the process of managing your website and other digital processes, by keeping an eye on everything for you! So that you can use your time more efficently.</p>
             </div>
           </div>
         </div>
