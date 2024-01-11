b9a44c6068103ff9ec62f3ad8d9eb52adfa2ce5b
Dominik Ucher <dominikucher@hotmail.com>
2024-01-11 15:34:30 +0100

    little more added

--- api/index.js
+++ api/index.js
@@ -6,9 +6,14 @@ import contactRoutes from './routes/contact.js'
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
+app.use(cookieParser());
 app.use("/api/contact", contactRoutes)
 
 
-app.listen(8800,()=>{
+app.listen(8801,()=>{
     console.log("Connected!")
 })
