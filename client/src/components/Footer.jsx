d6cff4d7efcf0673d59c6d40e52003d9251e9c0a
Dominik Ucher <dominikucher@hotmail.com>
2024-02-06 15:28:29 +0100

    Navbar and footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,54 @@
+import React, { useEffect, useState } from 'react';
 'use client';
-import React from 'react'
-import { Footer } from 'flowbite-react';
-import Logo_w_Text from '../../img/logo_with_text.png'
 import { Link } from 'react-router-dom';
+import axios from 'axios'
+import { FaRegCopyright } from "react-icons/fa6";
 
-export default function FooterWithLogo() {
-  return (
-    <div className='bg-black'>
-    <Footer container className='bg-black'>
-      <div className="w-full text-center">
-        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-          {/* <Link to="/"><Footer.Brand alt="Logo" className="h-12" src={Logo_w_Text} /></Link> */}
-          <Footer.LinkGroup>
-            {/* <Footer.Link href="#" className='text-white'>About</Footer.Link>
-            <Footer.Link href="#" className='text-white'>Privacy Policy</Footer.Link>
-            <Footer.Link href="#" className='text-white'>Licensing</Footer.Link>
-            <Footer.Link href="#" className='text-white'>Contact</Footer.Link> */}
-          </Footer.LinkGroup>
+const FooterTemp = () => {
+  const axiosInstance = axios.create({baseURL: import.meta.env.VITE_REACT_APP_API_URL,});
+
+
+    return (
+      <>
+      <div className='md:block hidden '>
+      <div className='flex flex-col h-auto bg-black overflow-hidden flex-shrink-0'>
+        <div className='text-md text-white flex pt-6 pb-6'>
+          {/* <Link to="/about" className='hover:underline ml-auto'>About</Link>
+          <Link to="/contact" className='hover:underline ml-4'>Contact</Link> */}
+          {/* <Link to="https://www.facebook.com/iltrondfotball" className='hover:underline ml-4'><FaFacebook color="white" className='w-6 h-auto'/></Link>
+          <Link to="https://www.instagram.com/il_trondfotball/" className='hover:underline px-4'><FaInstagram color="white" className='w-6 h-auto'/></Link>  */}
         </div>
-        <Footer.Divider className='bg-white' />
-        <Footer.Copyright by="RaindropCoding™" year={2024} className='text-white'/>
+        <div className='flex justify-center'>
+        <div className="w-11/12 h-px bg-white"></div>
+        </div>
+        <div className='flex justify-center text-md text-gray-400 pt-6 pb-6'>
+          <FaRegCopyright color="gray" className="mr-2 pt-1 h-auto" />
+          <Link to="/" className='hover:underline'>2024 Raindrop Coding</Link>
+        </div>
       </div>
-    </Footer>
-    </div>
-  )
+      </div>
+
+      <div className='block md:hidden'>
+        <div className='flex flex-col h-auto bg-black overflow-hidden flex-shrink-0'>
+          <div className='text-md text-white flex flex-col p-4'>
+            <div className='flex flex-col items-center mt-4'>
+              {/* <Link to="/about" className='hover:underline mb-2'>About</Link>
+              <Link to="/contact" className='hover:underline mb-2'>Contact</Link> */}
+              {/* <Link to="https://www.facebook.com/iltrondfotball" className='hover:underline mb-2'><FaFacebook color="white" className='w-6 h-auto'/></Link>
+              <Link to="https://www.instagram.com/il_trondfotball/" className='hover:underline mb-2'><FaInstagram color="white" className='w-6 h-auto'/></Link> */}
+            </div>
+          </div>
+          <div className='flex justify-center'>
+          <div className="w-11/12 h-px bg-white"></div>
+          </div>
+          <div className='flex justify-center text-md text-gray-400 pt-6 pb-6'>
+          <FaRegCopyright color="gray" className="mr-2 pt-1 h-auto" />
+          <Link to="/" className='hover:underline'>2024 Raindrop Coding</Link>
+        </div>
+        </div>
+      </div>
+      </>
+    )
 }
 
-
+export default FooterTemp
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import { Navbar } from 'flowbite-react';
-import { Link } from 'react-router-dom';
-import Logo_w_Text from '../../img/logo_with_text.png';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+'use client';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import Logo from '../../img/logo_with_text.png'
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { BsLayoutSidebarInset } from "react-icons/bs";
+
 
 export default function DefaultNavbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setSidebarOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
+
+
+
+
+
+
+
+
+
   return (
-    <Navbar fluid className="sticky top-0 z-10 bg-black">
-      <Link to="/">
-        <Navbar.Brand>
-          <LazyLoadImage alt="Logo" className="mr-3 h-12 sm:h-16" src={Logo_w_Text} />
-        </Navbar.Brand>
-      </Link>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Link to="/" className='text-white'><Navbar.Link>Home</Navbar.Link></Link>
-        <Link to="/about" className='text-white'><Navbar.Link>About</Navbar.Link></Link>
-        {/* Uncomment the line below if you have a "Projects" route */}
-        {/* <Link to="/projects" className='text-white'><Navbar.Link>Projects</Navbar.Link></Link> */}
-        <Link to="/contact" className='text-white'><Navbar.Link>Contact</Navbar.Link></Link>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+    // <div className="sticky top-0 z-50 bg-gray-800">
+    <div className="main-container" style={{ overflowX:'hidden'}} ref={sidebarRef}>
+    <div className='flex items-center bg-black h-auto w-screen border-b-2 border-white'>
+      <div className='flex items-center p-4'>
+        <Link className="flex items-center" to="/">
+        <img className="h-12" src={Logo} alt="" />
+        </Link>
+      </div>
+
+      <div className='invisible md:visible flex items-center absolute right-0 p-4'>
+        <Link className='text-sm text-white p-2' to="/">Home</Link>
+        <Link className='text-sm text-white p-2' to="/about">About</Link>
+        <Link className='text-sm text-white p-2' to="/contact">Contact</Link>
+        <button className='hover:border-2 rounded-2xl p-2 md:invisible visible' onClick={() => setSidebarOpen(!sidebarOpen)}><p><BsLayoutSidebarInset className="h-6 w-auto" color="white"/></p></button>
+
+      </div>
+    </div>
+    
+      <div className='md:w-1/3 w-3/4 h-auto bg-black relative rounded-br-3xl border-r-2 border-b-2 border-white' style={{ position: 'absolute', zIndex: '9999', left: sidebarOpen ? '0' : '-100%', transition: 'left 1s ease-in-out' }}>
+        <div>
+          <div className='visible md:invisible md:h-0 h-auto flex flex-col jusitfy-center items-center border-b-2'>
+          <Link className='text-sm text-white p-2' to="/">Home</Link>
+          <Link className='text-sm text-white p-2' to="/about">About</Link>
+          <Link className='text-sm text-white p-2' to="/contact">Contact</Link> 
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
+
+
