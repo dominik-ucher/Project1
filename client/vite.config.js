e9065226a69a01e711ebf643f1cabab7571f6111
Dominik Ucher <dominikucher@hotmail.com>
2024-08-13 20:49:49 +0200

    Fixed yarn build, back to the old days

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +12,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react(), ssr({prerender: true})],
+  plugins: [react()],
 })
