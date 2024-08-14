deea8024128b59fc6ac67f28b2f3ed9054e4b0a6
Dominik Ucher <dominikucher@hotmail.com>
2024-08-14 16:10:07 +0200

    Back to basics

--- client/src/entry-server.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from './App.jsx';
-
-export function render() {
-  return renderToString(<App />);
-}
--- client/vite.config.js
+++ client/vite.config.js
@@ -13,19 +13,4 @@ export default defineConfig({
     }
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        client: './src/entry-client.jsx',
-        server: './src/entry-server.jsx'
-      },
-      output: {
-        dir: 'dist',
-        format: 'esm'
-      }
-    }
-  },
-  ssr: {
-    noExternal: ['react', 'react-dom']
-  },
 })
